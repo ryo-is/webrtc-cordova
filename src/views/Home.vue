e05ed36180697e98b06e8628a34ad0e0c25d5698
ryo-is <ryosuke.izumi62@gmail.com>
2020-07-25 11:53:56 +0900

    add count demo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <div class="count">count: {{ state.count }}</div>
+    <div class="double">double: {{ state.double }}</div>
+    <button @click="countUp">CountUp</button>
+    <button @click="clear">Clear</button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, reactive, computed } from 'vue'
+
+type State = {
+  count: number
+  double: number
+}
 
 export default defineComponent({
-  name: 'Home',
+  name: 'home',
+  setup() {
+    const state: State = reactive({
+      count: 0,
+      double: computed<number>(() => state.count * 2),
+    })
+
+    const countUp = () => {
+      state.count++
+    }
+
+    const clear = () => {
+      state.count = 0
+    }
+
+    return {
+      state,
+      countUp,
+      clear,
+    }
+  },
 })
 </script>
