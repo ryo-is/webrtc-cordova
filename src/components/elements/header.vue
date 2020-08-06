fdee698fdc0bace7d5cd1457605d1dd9fcf0339e
Ryosuke Izumi <ryosuke.izumi62@gmail.com>
2020-08-06 15:05:07 +0900

    add skyway

--- src/components/elements/header.vue
+++ src/components/elements/header.vue
@@ -1,14 +1,6 @@
 <template>
   <div class="fixed inset-x-0 top-0 flex items-center h-12 px-10 z-100">
-    <div class="mr-4 text-xl font-bold">Vue3 Demo</div>
-    <div
-      v-for="item in state.links"
-      :key="item.label"
-      class="mx-4 text-xs font-semibold cursor-pointer link"
-      @click="$router.push(item.to)"
-    >
-      {{ item.label }}
-    </div>
+    <div class="mr-4 text-xl font-bold">WebRTC Demo</div>
   </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,101 @@
 <template>
   <div class="home">
-    <counter-display />
-    <increment-button />
-    <decrement-button />
+    <video id="skyway-video" width="400" autoplay muted playsinline></video>
+    <p>{{ state.peerID }}</p>
+    <div class="mb-4">
+      <label class="block text-md font-bold mb-2" for="their-id">
+        theirID
+      </label>
+      <input
+        v-model="state.theirID"
+        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+        id="their-id"
+        type="text"
+        placeholder="theirID"
+      />
+    </div>
+    <button
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none"
+      type="button"
+      @click="onCall"
+    >
+      Call
+    </button>
+    <video id="their-video" width="400" autoplay muted playsinline></video>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import CounterDisplay from '@/components/counter/CounterDisplay.vue'
-import IncrementButton from '@/components/counter/IncrementButton.vue'
-import DecrementButton from '@/components/counter/DecrementButton.vue'
+import { defineComponent, onMounted, reactive } from 'vue'
+import Peer, { MediaConnection } from 'skyway-js'
+
+type State = {
+  peerID: string
+  theirID: string
+}
+
+const peer = new Peer({
+  key: process.env.VUE_APP_AKYWAY_APIKEY
+    ? process.env.VUE_APP_AKYWAY_APIKEY
+    : '',
+  debug: 3,
+})
+
+let localStream: MediaStream
 
 export default defineComponent({
   name: 'home',
-  components: {
-    CounterDisplay,
-    IncrementButton,
-    DecrementButton,
+  setup() {
+    const state: State = reactive({
+      peerID: '',
+      theirID: '',
+    })
+
+    const setEventListener = (mediaConnection: MediaConnection) => {
+      mediaConnection.on('stream', (stream) => {
+        // video要素にカメラ映像をセットして再生
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const videoElm = document.getElementById('their-video') as any
+        videoElm.srcObject = stream
+        videoElm.play()
+      })
+    }
+
+    const onCall = () => {
+      const mediaConnection = peer.call(state.theirID, localStream)
+      setEventListener(mediaConnection)
+    }
+
+    onMounted(() => {
+      // カメラ映像取得
+      navigator.mediaDevices
+        .getUserMedia({ video: true, audio: true })
+        .then((stream) => {
+          // 成功時にvideo要素にカメラ映像をセットし、再生
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          const videoElm = document.getElementById('skyway-video') as any
+          videoElm.srcObject = stream
+          videoElm.play()
+          // 着信時に相手にカメラ映像を返せるように、グローバル変数に保存しておく
+          localStream = stream
+        })
+        .catch((error) => {
+          // 失敗時にはエラーログを出力
+          console.error('mediaDevice.getUserMedia() error:', error)
+          return
+        })
+
+      peer.on('open', () => {
+        state.peerID = peer.id
+      })
+
+      peer.on('call', (mediaConnection) => {
+        mediaConnection.answer(localStream)
+        setEventListener(mediaConnection)
+      })
+    })
+
+    return { state, onCall }
   },
 })
 </script>
